eea84c70d151ee3e7283a30de1c50710b3449acc
Hieuln99 <87683551+Hieuln99@users.noreply.github.com>
2021-10-21 09:42:03 +0700

    first time
    uhm

--- migrations/Version20211020021915.php
+++ migrations/Version20211020021915.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211019143306 extends AbstractMigration
+final class Version20211020021915 extends AbstractMigration
 {
     public function getDescription(): string
     {
