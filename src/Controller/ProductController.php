0d01f70b4866a75d1dd1414da2450f99341346ac
Hieuln99 <87683551+Hieuln99@users.noreply.github.com>
2021-10-22 21:42:06 +0700

    hieu

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'index')]
+    public function index(): Response
+    {
+        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
+        return $this->render('product/index.html.twig', ['products' =>$products]); 
+    }
+
+    #[Route('/product/detail/{id}', name: 'detail')]
+    public function detail($id): Response
+    {
+       $product = $this ->getDoctrine()->getRepository(Product::class)->find($id);
+       if($product==null){
+           $this->addFlash('Error','Book is not existed!');
+           return $this->redirectToRoute('index');
+       }
+       else{
+           return $this->render('product/detail.html.twig', ['product'=>$product]);
+       }
+    }
+    
+    #[Route('/product/delete/{id}', name: 'delete')]
+    public function delete($id): Response
+    {
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        if($product==null){
+            $this->addFlash('Error','Product is not existed!');
+        }
+        else{
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($id);
+            $this->addFlash('Error','Delete successfully');
+        }
+        return $this->redirectToRoute('index');
+    }
+
+    #[Route('/product/add', name: 'add')]
+    public function add(Request $request): Response
+    {
+       $product = new Product();
+       $form = $this->createForm(Product::class,$product);
+       $this->$form->handleRequest($request);
+       
+       if ($form->isSubmitted() && $form->isValid()) { 
+           
+       }
+    }
+
+    #[Route('/product/edit/{id}', name: 'edit')]
+    public function edit(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $img;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="products")
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Customer::class, mappedBy="products")
+     */
+    private $customers;
+
+    public function __construct()
+    {
+        $this->customers = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(?string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Customer[]
+     */
+    public function getCustomers(): Collection
+    {
+        return $this->customers;
+    }
+
+    public function addCustomer(Customer $customer): self
+    {
+        if (!$this->customers->contains($customer)) {
+            $this->customers[] = $customer;
+            $customer->addProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCustomer(Customer $customer): self
+    {
+        if ($this->customers->removeElement($customer)) {
+            $customer->removeProduct($this);
+        }
+
+        return $this;
+    }
+}
