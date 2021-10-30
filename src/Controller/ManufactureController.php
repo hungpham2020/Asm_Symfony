1c98e117ab7b2cdc05f1bb138b3d44fb27fe02ac
hungpham2020 <81425170+hungpham2020@users.noreply.github.com>
2021-10-30 08:25:47 +0700

    final

--- src/Controller/ManufactureController.php
+++ src/Controller/ManufactureController.php
@@ -7,6 +7,7 @@ use App\Form\ManufactureType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 /**
  * @IsGranted("ROLE_ADMIN")
@@ -82,7 +83,7 @@ class ManufactureController extends AbstractController
            return $this->redirectToRoute('manufacture');
         }
 
-        return $this->render("manufacture/add.html.twig",["form" => $form->createView()]);
+        return $this->render("manufacture/edit.html.twig",["form" => $form->createView()]);
     }
 }
 
